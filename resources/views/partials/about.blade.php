a8f238bcf0c52e6fecb33e5ec2cc3eefae2a3a94
Antonio Carlos Ribeiro <acr@antoniocarlosribeiro.com>
2015-07-31 17:52:24 -0300

    Change about text

--- resources/views/partials/about.blade.php
+++ resources/views/partials/about.blade.php
@@ -14,7 +14,13 @@
     <div class="container-fluid wow fadeIn">
         <div class="col-lg-10 col-lg-offset-1">
             <div class="row">
-                <p style="text-align: center;">Criado em 1998 e instalado em 2003, o Parlamento Juvenil é um projeto da Assembleia Legislativa do Estado do Rio de Janeiro (ALERJ) em parceria com a Secretaria de Estado de Educação (SEEDUC) e a Secretaria de Estado de Esporte, Lazer e Juventude (SEELJE), e envolve as escolas estaduais dos 92 municípios do Estado do Rio. O objetivo é aproximar o jovem da política, com intenção apartidária e totalmente autônoma. O projeto segue os mesmos moldes de um parlamento convencional, com direito à escolha da Mesa Diretora, Regimento Interno e votação em plenário. Durante uma semana, os estudantes passam a ter as mesmas atribuições de um deputado estadual e discutem e aprimoram projetos de lei de sua própria autoria. Nos últimos quatro anos, 252 parlamentares juvenis foram eleitos em suas cidades para participar do projeto no Palácio Tiradentes. Podem participar todos os estudantes selecionados nas cerca de 1.300 escolas da rede pública, a partir dos 14 anos, quantas vezes quiser, desde que não ultrapassem os 18 anos na data da inscrição. Os deputados juvenis são escolhidos pelo voto direto dos próprios colegas de escola, em dois turnos de votação. A capacitação dos eleitos acontece entre os meses de setembro e outubro, sob a orientação de uma coordenação geral da Alerj. O projeto, de autoria do deputado Jorge Picciani (PMDB), presidente da Alerj, chega a sua 9ª edição em 2015. </p>
+                <p style="text-align: center;">
+                    O Parlamento Juvenil é um projeto da Assembleia Legislativa do Rio de Janeiro (Alerj), apartidário e autônomo, cujo objetivo é aproximar o jovem da política. Instalado em 2003, envolve as 1.300 escolas estaduais dos 92 municípios do Estado e chega à nona edição em 2015.
+
+                    A iniciativa, de autoria do deputado Jorge Picciani (PMDB), presidente da Alerj, segue os moldes de um parlamento convencional. Eleitos pelos próprios colegas de escola, por voto direto, os parlamentares juvenis têm as mesmas atribuições dos deputados estaduais. Durante uma semana, eles apresentam, discutem e aprimoram projetos de lei.
+
+                    Nos últimos quatro anos, 252 jovens foram escolhidos. Para participar da seleção, é preciso ter entre 14 e 17 anos e ser aluno da rede pública de ensino.
+                </p>
             </div>
         </div>
     </div>
