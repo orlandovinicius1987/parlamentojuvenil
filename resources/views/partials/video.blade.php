fbb94e4d7e1e47b26caaab8ae12a29d41550eec9
Antonio Carlos Ribeiro <acr@antoniocarlosribeiro.com>
2015-08-01 18:41:21 -0300

    Add video

---
+++ resources/views/partials/video.blade.php
@@ -0,0 +1,14 @@
+<!-- video -->
+
+<section class="about-grid" id="video">
+    <div class="container-fluid wow fadeIn">
+        <div class="col-lg-10 col-lg-offset-1 video-container">
+            <iframe type="text/html"
+                    src="http://www.youtube.com/embed/AB1MeT7MKds"
+                    frameborder="0">
+            </iframe>
+        </div>
+    </div>
+</section>
+
+
