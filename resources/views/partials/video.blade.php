0d86cbb67e41fbef38c5c6e324f4b974fe92d308
Antonio Carlos Ribeiro <acr@antoniocarlosribeiro.com>
2015-08-04 21:33:07 -0300

    Fix video

--- resources/views/partials/video.blade.php
+++ resources/views/partials/video.blade.php
@@ -2,13 +2,13 @@
 
 <section class="about-grid" id="video">
     <div class="container-fluid wow fadeIn">
-        <div class="col-lg-10 col-lg-offset-1 video-container">
+        <div class="col-lg-10 col-lg-offset-1 video-container text-center">
             <iframe type="text/html"
                     src="http://www.youtube.com/embed/AB1MeT7MKds"
-                    frameborder="0">
+                    frameborder="0"
+                    width="640"
+                    height="390">
             </iframe>
         </div>
     </div>
 </section>
-
-
