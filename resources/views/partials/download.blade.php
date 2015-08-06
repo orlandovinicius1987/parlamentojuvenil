c930a04fa8a466c824b0aa08f3de2d7f628a1f78
Antonio Carlos Ribeiro <acr@antoniocarlosribeiro.com>
2015-08-06 02:19:16 -0300

    Download view

---
+++ resources/views/partials/download.blade.php
@@ -0,0 +1,24 @@
+<!-- callout -->
+<section class="callout" id="download">
+    <div class="container-fluid text-vertical text-center">
+        <div class="col-sm-10 col-sm-offset-1 wow fadeInDownBig">
+            <h1 class="heading">Download</h1>
+            <p>Você pode precisar desses documentos</p>
+            <div class="row">
+                <a
+                    href="{{ route('download', ['regimento-interno.pdf']) }}"
+                    class="btn border-button">
+                        REGIMENTO INTERNO <i class="fa fa-cloud-download"></i>
+                </a>
+            </div>
+
+            <div class="row">
+                <a
+                    href="{{ route('download', ['manual-do-parlamentar.pdf']) }}"
+                    class="btn border-button">MANUAL DO PARLAMENTAR <i class="fa fa-cloud-download"></i>
+                </a>
+            </div>
+        </div>
+    </div>
+</section>
+
