56156154e92878c4cf5f2651a887a3a518dd8439
Antonio Carlos Ribeiro <acr@antoniocarlosribeiro.com>
2015-08-07 03:02:03 -0300

    Add some checks

--- public/js/app.js
+++ public/js/app.js
@@ -4,18 +4,19 @@ var subscribe = new Vue({
     el: '#subscribe',
 
     data: {
-        name: '',
-        city: '',
-        school: '',
-        registration: '',
-        gender: '',
-        email: '',
-        phone: '',
-        schools: emptySchool
+        address: null,
+        address_complement: null,
+        address_neighborhood: null,
+        address_city: null,
+        cpf: null,
+        schools: emptySchool,
+        cpfValid: false,
+        zipValid: false
     },
 
     methods: {
-        fetchSchools: function() {
+        fetchSchools: function()
+        {
             if (this.city)
             {
                 this.$data.schools.length = 0;
@@ -23,6 +24,43 @@ var subscribe = new Vue({
 
                 this.$http.get('/schools/' + this.city, function(schools) {
                     schools.forEach(newSchool);
+                });
+            }
+        },
+
+        checkCpf: function()
+        {
+            var cpf = jQuery('#cpf').val();
+
+            cpf = cpf.split('.').join("");
+            cpf = cpf.split('-').join("");
+            cpf = cpf.split('_').join("");
+
+            this.cpfValid = TestaCPF(cpf);
+
+            console.log(this.cpfValid);
+        },
+
+        checkZip: function()
+        {
+            var zip = jQuery('#zip_code').val();
+
+            zip = zip.split('.').join("");
+            zip = zip.split('-').join("");
+            zip = zip.split('_').join("");
+
+            if (zip.length == 8)
+            {
+                this.zipValid = true;
+
+                this.$http.get('http://viacep.com.br/ws/'+zip+'/json/', function(zip)
+                {
+                    if (zip.localidade)
+                    {
+                        this.address_city = zip.localidade;
+                        this.address = zip.logradouro;
+                        this.address_neighborhood = zip.bairro;
+                    }
                 });
             }
         }
