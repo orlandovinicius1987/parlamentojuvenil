70f5c86dafe3041c09c2c906f176a918ba76d147
Antonio Carlos Ribeiro <acr@antoniocarlosribeiro.com>
2015-08-09 18:01:35 -0300

    Remove stuff

--- resources/views/admin/partials/menu.blade.php
+++ resources/views/admin/partials/menu.blade.php
@@ -15,21 +15,21 @@
             <a href="{{ route('home') }}"><i class="fa fa-home fa-fw"></i></a>
         </li>
 
-        <li class="dropdown">
-            <a class="dropdown-toggle" data-toggle="dropdown" href="#">
-                <i class="fa fa-user fa-fw"></i>  <i class="fa fa-caret-down"></i>
-            </a>
-            <ul class="dropdown-menu dropdown-user">
-                <li><a href="#"><i class="fa fa-user fa-fw"></i> User Profile</a>
-                </li>
-                <li><a href="#"><i class="fa fa-gear fa-fw"></i> Settings</a>
-                </li>
-                <li class="divider"></li>
-                <li><a href="{{ route('auth.logout') }}"><i class="fa fa-sign-out fa-fw"></i> Logout</a>
-                </li>
-            </ul>
-            <!-- /.dropdown-user -->
-        </li>
+        {{--<li class="dropdown">--}}
+            {{--<a class="dropdown-toggle" data-toggle="dropdown" href="#">--}}
+                {{--<i class="fa fa-user fa-fw"></i>  <i class="fa fa-caret-down"></i>--}}
+            {{--</a>--}}
+            {{--<ul class="dropdown-menu dropdown-user">--}}
+                {{--<li><a href="#"><i class="fa fa-user fa-fw"></i> User Profile</a>--}}
+                {{--</li>--}}
+                {{--<li><a href="#"><i class="fa fa-gear fa-fw"></i> Settings</a>--}}
+                {{--</li>--}}
+                {{--<li class="divider"></li>--}}
+                {{--<li><a href="{{ route('auth.logout') }}"><i class="fa fa-sign-out fa-fw"></i> Logout</a>--}}
+                {{--</li>--}}
+            {{--</ul>--}}
+            {{--<!-- /.dropdown-user -->--}}
+        {{--</li>--}}
         <!-- /.dropdown -->
     </ul>
     <!-- /.navbar-top-links -->
@@ -37,17 +37,17 @@
     <div class="navbar-default sidebar" role="navigation">
         <div class="sidebar-nav navbar-collapse">
             <ul class="nav" id="side-menu">
-                <li class="sidebar-search">
-                    <div class="input-group custom-search-form">
-                        <input type="text" class="form-control" placeholder="{{'captions.search'}}...">
-                                <span class="input-group-btn">
-                                <button class="btn btn-default" type="button">
-                                    <i class="fa fa-search"></i>
-                                </button>
-                            </span>
-                    </div>
-                    <!-- /input-group -->
-                </li>
+                {{--<li class="sidebar-search">--}}
+                    {{--<div class="input-group custom-search-form">--}}
+                        {{--<input type="text" class="form-control" placeholder="{{'captions.search'}}...">--}}
+                                {{--<span class="input-group-btn">--}}
+                                {{--<button class="btn btn-default" type="button">--}}
+                                    {{--<i class="fa fa-search"></i>--}}
+                                {{--</button>--}}
+                            {{--</span>--}}
+                    {{--</div>--}}
+                    {{--<!-- /input-group -->--}}
+                {{--</li>--}}
                 <li>
                     <a href="{{ route('home') }}"><i class="fa fa-home fa-fw"></i> Hotsite</a>
                 </li>
