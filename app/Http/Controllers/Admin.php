c6d105e3c5837540817e67b5d707e3bc8c34836b
Antonio Carlos Ribeiro <acr@antoniocarlosribeiro.com>
2015-08-10 19:13:19 -0300

    Correctly filter cities

--- app/Http/Controllers/Admin.php
+++ app/Http/Controllers/Admin.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers;
 
+use \DB;
 use App\Data\Entities\School;
 use App\Data\Entities\Subscription;
 use App\Http\Controllers\Controller as BaseController;
@@ -18,7 +19,7 @@ class Admin extends BaseController
 		return view('admin.city')
 				->with('city', $city)
 				->with('subscriptions', Subscription::where('city', $city)->get())
-				->with('schools', School::where('city', 'like', strtoupper($city))->get())
+				->with('schools', School::where('city', 'like', DB::raw("UPPER('".$city."')"))->get())
 		;
 	}
 }
