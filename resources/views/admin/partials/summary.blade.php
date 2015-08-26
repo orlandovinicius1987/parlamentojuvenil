36acc83ef6558d2454cd0c1c83fdadfb11f73b88
Antonio Carlos Ribeiro <acr@antoniocarlosribeiro.com>
2015-08-26 01:02:05 -0300

    Align to center

--- resources/views/admin/partials/summary.blade.php
+++ resources/views/admin/partials/summary.blade.php
@@ -1,18 +1,18 @@
 <table class="table table-striped table-bordered table-hover">
     <thead>
         <tr>
-            <th>Inscrições</th>
-            <th>Municípios inscritos</th>
-            <th>Municípios não-inscritos</th>
-            <th>Canceladas/Duplicadas</th>
+            <th class="text-center">Inscrições</th>
+            <th class="text-center">Municípios inscritos</th>
+            <th class="text-center">Municípios não-inscritos</th>
+            <th class="text-center">Canceladas/Duplicadas</th>
         </tr>
     </thead>
     <tbody>
         <tr>
-            <td>@{{ total }}</td>
-            <td>@{{ citiesIn }}</td>
-            <td>@{{ citiesOut }}</td>
-            <td>@{{ cancelled }}</td>
+            <td class="text-center">@{{ total }}</td>
+            <td class="text-center">@{{ citiesIn }}</td>
+            <td class="text-center">@{{ citiesOut }}</td>
+            <td class="text-center">@{{ cancelled }}</td>
         </tr>
     </tbody>
 </table>
