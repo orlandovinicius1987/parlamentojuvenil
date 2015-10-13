e021cf77d121b91c42091045b833eab094c41538
Antonio Carlos Ribeiro <acr@antoniocarlosribeiro.com>
2015-10-13 15:39:15 -0300

    Make button look better

--- resources/views/partials/deputy-entrance.blade.php
+++ resources/views/partials/deputy-entrance.blade.php
@@ -1,3 +1,7 @@
 <h2>Você foi eleito Parlamentar Juvenil de 2015?</h2>
 
-<a href="http://www.alerj.rj.gov.br/parlamentojuvenil/projetolei/logar.aspx" class="btn btn-xl btn-primary">Clique aqui e cadastre seu Projeto de Lei</a>
+<a href="http://www.alerj.rj.gov.br/parlamentojuvenil/projetolei/logar.aspx" class="btn btn-lg btn-primary">Clique aqui e cadastre seu Projeto de Lei</a>
+
+<br/>
+<br/>
+<br/>
