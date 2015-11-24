0f8618eb07a1068d189596dc614013b18a913ac7
Antonio Carlos Ribeiro <acr@antoniocarlosribeiro.com>
2015-11-24 16:11:38 +0000

    Change calendar to board

---
+++ resources/views/partials/board.blade.php
@@ -0,0 +1,22 @@
+<h1 id="calendar" style="text-align: center;">MESA DIRETORA</h1>
+
+<div class="row text-center">
+    <h3 class="calendar-title">Presidente</h3>
+    <p>Lara Carvalho (Duas Barras)</p>
+
+    <h3 class="calendar-title">Vice-presidente</h3>
+    <p>Enzo Carvalho (Resende)</p>
+
+    <h3 class="calendar-title">2ª vice-presidente</h3>
+    <p>Luiza Barros (Iguaba Grande)</p>
+
+    <h3 class="calendar-title">1ª secretária</h3>
+    <p>Sárya Martins (Quissamã)</p>
+
+    <h3 class="calendar-title">2º secretário</h3>
+    <p>Lucas Mota (Santo Antônio de Pádua)</p>
+
+    <h3 class="calendar-title">Suplentes</h3>
+    <p>João Pedro Valente (São Pedro da Aldeia)</p>
+    <p>Maria Luíza (Belford Roxo)</p>
+</div>
