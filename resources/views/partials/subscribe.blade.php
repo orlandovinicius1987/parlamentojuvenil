0a081d0a99ebdb2feea30b04164832963b750510
Fabiano Albernaz <fabiano.albernaz@gmail.com>
2016-05-18 16:59:22 +0000

    more home CSS

--- resources/views/partials/subscribe.blade.php
+++ resources/views/partials/subscribe.blade.php
@@ -1,6 +1,6 @@
 <div id="cadastro">
     <h2>Inscreva-se</h2>
-    <p>Alunos da rede estadual de ensino</p>
+    <p>Alunos da rede estadual de ensino, favor preencher o formulário abaixo, para proceder com sua inscrição</p>
 
     @include('partials.subscribe-form')
 </div>
