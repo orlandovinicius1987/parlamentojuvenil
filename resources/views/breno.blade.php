8694a788267588fbb9f4fd8a64b1854a5c51fb61
Fabiano Albernaz <fabiano.albernaz@gmail.com>
2016-05-19 21:46:38 +0000

    depoimentos css

--- resources/views/breno.blade.php
+++ resources/views/breno.blade.php
@@ -328,7 +328,7 @@
                     </div>
 
                     <div class="row">
-                        <div class="col-xs-12 col-md-4 amarelo texto-preto">
+                        <div class="col-xs-12 col-md-4 quadro-amarelo texto-preto">
                             <div class="titulo borda-preta text-center">Loren Post Title</div>
                             <div class="thumbnail">
                                 <a href="#">
@@ -349,7 +349,7 @@
                                 </div>
                             </div>
                         </div>
-                        <div class="col-xs-12 col-md-4 amarelo texto-preto">
+                        <div class="col-xs-12 col-md-4 quadro-amarelo texto-preto">
                             <div class="titulo borda-preta text-center">Loren Post Title</div>
                             <div class="thumbnail">
                                 <a href="#">
@@ -370,7 +370,7 @@
                                 </div>
                             </div>
                         </div>
-                        <div class="col-xs-12 col-md-4 amarelo texto-preto">
+                        <div class="col-xs-12 col-md-4 quadro-amarelo texto-preto">
                             <div class="titulo borda-preta text-center">Loren Post Title</div>
                             <div class="thumbnail">
                                 <a href="#">
@@ -391,12 +391,19 @@
                                 </div>
                             </div>
                         </div>
+                    </div>
+                    <br>
+                    <div class="row text-center">
+                        <div class="col-md-5 quadro-amarelo hidden-xs hidden-sm"><div class="borda-preta">&nbsp;</div></div>
+                        <div class="col-md-2 col-xs-12"><div class="quadro-vermelho borda-preta">Ler mais Notícias</div></div>
+                        <div class="col-md-5 quadro-amarelo hidden-xs hidden-sm"><div class="borda-preta">&nbsp;</div></div>
                     </div>
 
-                <div class="row text-center">
-                        <button type="button" class="btn btn-default">Ver mais notícias</button>
-                    </div>
-                </div>
+                {{--<div class="row text-center">--}}
+                        {{--<button type="button" class="btn btn-default">Ver mais notícias</button>--}}
+                    {{--</div>--}}
+                {{--</div>--}}
+
             </div>
         </div>
 
@@ -2263,12 +2270,13 @@
                 {{--<img class="img-responsive" src="/pj2016/images/twit.png" alt="twit">--}}
             {{--</div>--}}
             <div class="row">
-                <div class="col-sm-6 col-sm-offset-3">
-                    <div class="text-center carousel-inner center-block">
+                <div class="col-xs-12 col-md-8 col-md-offset-2">
+                    <div class="text-center carousel-inner borda-preta">
                         <div class="item active">
-                            <img src="/pj2016/images/depoimentos/depoimento1.png" alt="">
-                            <p>In hac habitasse platea dictumst. Nam pulvinar, odio sed rhoncus suscipit </p>
-                            <a href="#">http://t.co/yY7s1IfrAb 2 days ago</a>
+                            <div class="pull-left"><img src="/pj2016/images/depoimentos/depoimento1.png" alt=""></div>
+                            <div class="text-center"><p>In hac habitasse platea dictumst. Nam pulvinar, odio sed rhoncus suscipit </p>
+                                <a href="#">http://t.co/yY7s1IfrAb 2 days ago</a></div>
+
                         </div>
                         <div class="item">
                             <img src="/pj2016/images/depoimentos/depoimento2.png" alt="">
