797eb0ab008ba4490c15f83d188f95e6169f93b6
Fabiano Albernaz <fabiano.albernaz@gmail.com>
2016-05-20 22:37:53 +0000

    90% ready

--- resources/views/2016/partials/downloads.blade.php
+++ resources/views/2016/partials/downloads.blade.php
@@ -3,38 +3,42 @@
         <div class="row">
             <div class="col-xs-12 col-md-12 text-center">
                 {{--<img src="/pj2016/images/download.png">--}}
-                <h2>Downloads</h2>
-                <br>
-                <p class="caixa-vermelha" style="margin-top: -9px;">Você pode precisar desses documentos</p>
-                <div class="row text-center">
-                    <div class="col-xs-12 col-lg-4">
-                        <div class="classWithPad caixa-amarela">
-                            <a href="http://www.parlamento-juvenil.rj.gov.br/download/regimento-interno.pdf" class="btn border-button">
-                                <img src="/pj2016/images/regimento.png" class="img-responsive">
-                            </a>
-                            <p>Regimento Interno</p>
-                        </div>
-                    </div>
+                <h2>&nbsp;</h2>
+                <p class="caixa-preta">Downloads</p>
+                <p class="caixa-vermelha">Você pode precisar desses documentos</p>
+            </div>
+        </div>
 
-                    <div class="col-xs-12 col-lg-4">
-                        <div class="classWithPad caixa-amarela">
-                            <a href="http://www.parlamento-juvenil.rj.gov.br/download/anexo-ii-calendario-de-atividades-9a-edicao.pdf" class="btn border-button">
-                                <img src="/pj2016/images/calendario.png" class="img-responsive">
-                            </a>
-                            <p>Calendário de Atividades</p>
-                        </div>
+        <div class="container">
+            <div class="row text-center">
+                <div class="col-xs-12 col-lg-4">
+                    <div class="classWithPad caixa-amarela">
+                        <a href="http://www.parlamento-juvenil.rj.gov.br/download/regimento-interno.pdf" class="btn border-button">
+                            <img src="/pj2016/images/regimento.png" class="img-responsive">
+                        </a>
+                        <p>Regimento Interno</p>
                     </div>
+                </div>
 
-                    <div class="col-xs-12 col-lg-4">
-                        <div class="classWithPad caixa-amarela">
-                            <a href="http://www.parlamento-juvenil.rj.gov.br/download/documentos-necessarios-para-participar-parlamento-juvenil.zip" class="btn border-button">
-                                <img src="/pj2016/images/documentos.png" class="img-responsive">
-                            </a>
-                            <p>Documentos Necessários para Participar</p>
-                        </div>
+                <div class="col-xs-12 col-lg-4">
+                    <div class="classWithPad caixa-amarela">
+                        <a href="http://www.parlamento-juvenil.rj.gov.br/download/anexo-ii-calendario-de-atividades-9a-edicao.pdf" class="btn border-button">
+                            <img src="/pj2016/images/calendario.png" class="img-responsive">
+                        </a>
+                        <p>Calendário de Atividades</p>
+                    </div>
+                </div>
+
+                <div class="col-xs-12 col-lg-4">
+                    <div class="classWithPad caixa-amarela">
+                        <a href="http://www.parlamento-juvenil.rj.gov.br/download/documentos-necessarios-para-participar-parlamento-juvenil.zip" class="btn border-button">
+                            <img src="/pj2016/images/documentos.png" class="img-responsive">
+                        </a>
+                        <p>Documentos para Participar</p>
                     </div>
                 </div>
             </div>
         </div>
     </div>
+
 </section><!--/#event-->
