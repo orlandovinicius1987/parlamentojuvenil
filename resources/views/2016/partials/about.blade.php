5ee0ca9b21a123b071ed88783239bcb26d75a506
Antonio Carlos Ribeiro <acr@antoniocarlosribeiro.com>
2016-05-20 13:58:34 -0300

    Move about

---
+++ resources/views/2016/partials/about.blade.php
@@ -0,0 +1,40 @@
+<section id="sobre">
+    <div class="container-fluid">
+        <div class="row">
+            <div class="col-xs-12 col-md-6">
+                {{--  Inscreva-se --}}
+                @include('partials.subscribe')
+
+            </div>
+            <div class="side">
+                <div class="col-xs-12 col-md-4 col-md-offset-1 bg">
+                    <div class="row">
+                        <div class="col-xs-12 col-md-12">
+                            <h3>E como funciona?</h3>
+
+                            <div class="content">
+                                <div>
+                                    <h4>1</h4>
+                                    <p>Cada município do Rio de Janeiro pode ser representado por um parlamentar juvenil. As eleições acontecem nas escolas, em dois turnos de votação.
+                                    </p>
+                                </div>
+
+                                <div>
+                                    <h4>2</h4>
+                                    <p>Os Deputados eleitos aprendem mais sobre sua função em oficinas de capacitação preparadas pela Alerj.
+                                    </p>
+                                </div>
+
+                                <div>
+                                    <h4>3</h4>
+                                    <p>Durante uma semana, os parlamentares apresentam, debatem e votam os projetos de sua autoria no plenário da Alerj. No fim, as escolhas são encaminhadas á apreciação do Governador, podendo ou não virar lei.
+                                    </p>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</section><!--/#about-->
