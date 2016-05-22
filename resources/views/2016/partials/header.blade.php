279654a878dd34683e62ab95177edbde7db34eaf
Antonio Carlos Ribeiro <acr@antoniocarlosribeiro.com>
2016-05-22 19:00:43 -0300

    Update database

--- resources/views/2016/partials/header.blade.php
+++ resources/views/2016/partials/header.blade.php
@@ -21,45 +21,8 @@
                         <img class="img-responsive" src="/pj2016/images/logo-pj.png" alt="Parlamento Juvenil">
                     </a>
                 </div>
-                <div class="collapse navbar-collapse">
-                    <ul class="nav navbar-nav navbar-right">
-                        <li class="scroll active"><a href="#home">Início</a></li>
-                        <li class="scroll"><a href="#sobre">Sobre</a></li>
-                        <li class="scroll"><a href="/inscricao">Inscrição</a></li>
 
-                        @if (App::environment('local'))
-                            <li class="scroll"><a href="#noticias">Notícias</a></li>
-                        @endif
-
-                        @if (App::environment('local'))
-                            <li class="scroll"><a href="#galeria">Galeria</a></li>
-                        @endif
-
-                        @if (App::environment('local'))
-                            <li class="dropdown">
-                                <!-- Link or button to toggle dropdown -->
-                                <a id="dLabel" href="" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">Anteriores <span class="caret"></span></a>
-                                <ul class="dropdown-menu" role="menu" aria-labelledby="dLabel">
-                                    <li><a href="/2015">2015</a></li>
-                                    <li><a tabindex="-1" href="">2014</a></li>
-                                    <li><a tabindex="-1" href="">2013</a></li>
-                                    <li class="divider"></li>
-                                    <li><a tabindex="-1" href="">Outras Edições</a></li>
-                                </ul>
-                            </li>
-                        @endif
-
-                        <li class="scroll"><a href="#downloads">Downloads</a></li>
-
-                        @if (App::environment('local'))
-                            <li class="scroll"><a href="#depoimentos">Depoimentos</a></li>
-                        @endif
-
-                        <!--<li class="no-scroll"><a href="#twitter">Twitter</a></li>
-                        <li><a class="no-scroll" href="#" target="_blank">PURCHASE TICKETS</a></li>-->
-                        <li class="scroll"><a href="#contato">Contato</a></li>
-                    </ul>
-                </div>
+                @include('2016.partials.menu-items')
             </div>
         </div>
     </div>
