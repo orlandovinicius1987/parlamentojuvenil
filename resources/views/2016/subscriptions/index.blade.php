2473a3f13046e01177114607dbeebc7907269087
Antonio Carlos Ribeiro <acr@antoniocarlosribeiro.com>
2016-05-22 21:50:08 -0300

    Fix subscriptions

--- resources/views/2016/subscriptions/index.blade.php
+++ resources/views/2016/subscriptions/index.blade.php
@@ -18,11 +18,7 @@
         <div class="container">
             <div class="row">
                 <div class="col-xs12">
-                    @if ($canSubscribe)
-                        @include('partials.subscribe-form')
-                    @else
-                        <h2>Você já está inscrito!</h2>
-                    @endif
+                    @include('partials.subscribe-form')
                 </div>
             </div>
         </div>
