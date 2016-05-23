c09805ea23947ea9774be4068b706e9b82bdf30f
Fabiano Albernaz <fabiano.albernaz@gmail.com>
2016-05-23 23:08:13 +0000

    video responsive

--- resources/views/2016/partials/video.blade.php
+++ resources/views/2016/partials/video.blade.php
@@ -1,3 +1,4 @@
-<section id="video" class="fundo-azul text-center">
-    <iframe width="560" height="315" src="https://www.youtube.com/embed/Jv-8FS-prn4" frameborder="0" allowfullscreen></iframe>
-</section>
+<div id="video" class="fundo-azul text-center">
+    <style>.embed-container { position: relative; padding-bottom: 56.25%; height: 0; overflow: hidden; max-width: 100%; } .embed-container iframe, .embed-container object, .embed-container embed { position: absolute; top: 0; left: 0; width: 100%; height: 100%; }</style>
+    <div class='embed-container'><iframe src='https://www.youtube.com/embed/Jv-8FS-prn4' frameborder='0' allowfullscreen></iframe></div>
+</div>
