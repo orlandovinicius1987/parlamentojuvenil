51554c363d93baedb057954a59c0ad3aba2a6fc0
Breno Trengrouse <brenot@gmail.com>
2016-06-28 19:07:01 +0000

    Clipping 2013

--- app/Data/Entities/Clipping.php
+++ app/Data/Entities/Clipping.php
@@ -7,6 +7,73 @@ class Clipping
     public static function all()
     {
         return [
+            2013 => [
+                [
+                    'id' => '',
+                    'title' => '',
+                    'lead' => '',
+                    'vehicle' => '',
+                    'file' => '',
+                    'url' => '',
+                ],
+                [
+                    'id' => '04/10/2013',
+                    'title' => 'Parlamentar Juvenil de Belford Roxo, Luan Peixoto, participa de curso de capacitação sobre atuação na Alerj',
+                    'lead' => 'Na última terça-feira (01 de Outubro), ocorreu o primeiro curso de capacitação do projeto Parlamento Juvenil, no Colégio Estadual José Leite Lopes, o Nave, na Tijuca, Zona Norte do Rio de Janeiro. 
+O encontro serviu para colocar as ideias e propostas nos moldes de um projeto de lei, além de conhecer as normas básicas do regimento interno da Assembleia Legislativa do Rio (Alerj).',
+                    'vehicle' => 'Notícias de Belford Roxo',
+                    'file' => '2013-11-11-belford-roxo',
+                    'url' => 'http://noticiasdebelfordroxo.blogspot.com.br/2013/10/parlamentar-juvenil-de-belford-roxo-luan-peixoto-alerj.html',
+                ],
+                [
+                    'id' => '16/10/2013',
+                    'title' => 'Jovens passam por treinamento do Parlamento Juvenil em Friburgo, RJ',
+                    'lead' => '',
+                    'vehicle' => 'RJ TV',
+                    'file' => '2013-10-16-tv-globo',
+                    'url' => 'http://g1.globo.com/rj/regiao-serrana/rjintertv-2edicao/videos/v/jovens-passam-por-treinamento-do-parlamento-juvenil-em-friburgo-rj/2893294/',
+                ],
+                [
+                    'id' => '11/11/2013',
+                    'title' => 'Notícias de Belford Roxo',
+                    'lead' => ' A 7ª edição do Parlamento Juvenil da Assembleia Legislativa do Rio de Janeiro (PJ-Alerj) foi instalada no início deste mês (04 de novembro). Luan Peixoto (19 anos), morador de Belford Roxo e aluno do 1º ano do ensinou médio do Colégio Estadual Presidente Kennedy, tomou posse como Deputado Juvenil.',
+                    'vehicle' => 'Notícias de Belford Roxo',
+                    'file' => '2013-10-04-belford-roxo',
+                    'url' => 'http://noticiasdebelfordroxo.blogspot.com.br/2013/11/belford-roxo-deputado-juvenil-luan-toma-posse-na-alerj.html',
+                ],
+                [
+                    'id' => '272',
+                    'title' => 'A nova geração do plenário',
+                    'lead' => 'Parlamento Juvenil chega à sua sétima edição e já tem até vereador entre seus ex-membros',
+                    'vehicle' => 'Jornal da Alerj',
+                    'file' => '2013_Jornal da Alerj_no272',
+                    'url' => '',
+                ],
+                [
+                    'id' => '273',
+                    'title' => 'Retorno ao Parlamento Juvenil',
+                    'lead' => 'Estudantes-deputados se reelegem e jovem quer voltar à presidência da Mesa Diretora',
+                    'vehicle' => 'Jornal da Alerj',
+                    'file' => '2013_Jornal da Alerj_no273',
+                    'url' => '',
+                ],
+                [
+                    'id' => '274',
+                    'title' => 'Juventude em Brasília',
+                    'lead' => 'Alerj recebe representantes fluminenses de projeto da Câmara dos Deputados voltado para estudantes',
+                    'vehicle' => 'Jornal da Alerj',
+                    'file' => '2013_Jornal da Alerj_no274',
+                    'url' => '',
+                ],
+                [
+                    'id' => '277',
+                    'title' => 'Renovação na política',
+                    'lead' => 'Sétima edição do Parlamento Juvenil tem como saldo a aprovação de uma PEC e três projetos de lei',
+                    'vehicle' => 'Jornal da Alerj',
+                    'file' => '2013_Jornal da Alerj_no277',
+                    'url' => '',
+                ],
+            ],
             2014 => [
                 [
                     'id' => 'nº292',
@@ -30,8 +97,16 @@ projetos em comissão e votaram proposições, escolhendo três iniciativas que 
                     'id' => '12/11/2014',
                     'title' => 'Adolescentes experimentam rotina de deputado por uma semana na Alerj',
                     'lead' => '',
-                    'vehicle' => 'O Globo RioD:\Projetos\parlamentojuvenil\public\files\apps\parlamentojuvenil\reportagens_edicoes_anteriores',
-                    'file' => '',
+                    'vehicle' => 'O Globo Rio',
+                    'file' => '2014-11-12-tv-globo',
+                    'url' => 'http://globotv.globo.com/infoglobo/o-globo-rio/v/adolescentes-experimentam-rotina-de-deputado-por-uma-semana-na-alerj/3758857/',
+                ],
+                [
+                    'id' => '12/11/2014',
+                    'title' => 'Adolescentes experimentam rotina de deputado por uma semana na Alerj',
+                    'lead' => '',
+                    'vehicle' => 'O Globo Rio',
+                    'file' => '2014-11-12-tv-globo',
                     'url' => 'http://globotv.globo.com/infoglobo/o-globo-rio/v/adolescentes-experimentam-rotina-de-deputado-por-uma-semana-na-alerj/3758857/',
                 ],
             ],
