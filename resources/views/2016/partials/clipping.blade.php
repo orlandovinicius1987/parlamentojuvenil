c9b479d0a9bfe2314af2414f04a681a7d0c59893
Antonio Carlos Ribeiro <acr@antoniocarlosribeiro.com>
2016-06-30 17:36:11 -0300

    Add year to title

--- resources/views/2016/partials/clipping.blade.php
+++ resources/views/2016/partials/clipping.blade.php
@@ -1,20 +1,22 @@
-<section id="clippings-anteriores" class="pinboards fundo-azul2">
-    <div class="container ">
-        <div class="row text-center">
-            <h2>Clipping Edição 2015</h2>
+@if (isset($clipping) && $clipping)
+    <section id="clippings-anteriores" class="pinboards fundo-azul2">
+        <div class="container ">
+            <div class="row text-center">
+                <h2>Clipping Edição {{ $clipping[0]['year'] }}</h2>
 
-            <section id="pinBoot">
-                @foreach($clipping as $item)
-                    <article class="white-panel"><a href="{{ $item['pdf'] }}"><img src="{{ $item['jpg'] }}" alt=""></a>
-                        <h4><a href="{{ $item['pdf'] }}">{{ $item['title'] }}</a></h4>
-                        <p>{{ $item['lead'] }}</p>
-                    </article>
-                @endforeach
-            </section>
-        </div>
+                <section id="pinBoot">
+                    @foreach($clipping as $item)
+                        <article class="white-panel"><a href="{{ $item['pdf'] }}"><img src="{{ $item['jpg'] }}" alt=""></a>
+                            <h4><a href="{{ $item['pdf'] }}">{{ $item['title'] }}</a></h4>
+                            <p>{{ $item['lead'] }}</p>
+                        </article>
+                    @endforeach
+                </section>
+            </div>
 
-        <div class="row text-center">
-            <button type="button" class="btn btn-default">Ver mais clippings</button>
+            <div class="row text-center">
+                <button type="button" class="btn btn-default">Ver mais clippings</button>
+            </div>
         </div>
-    </div>
-</section>
+    </section>
+@endif
