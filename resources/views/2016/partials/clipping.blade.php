2f0c912388e9c957a8a0094c58ffb438e5bbf262
Breno Trengrouse <brenot@gmail.com>
2016-07-01 18:36:14 +0000

    Ajustes Clippings

--- resources/views/2016/partials/clipping.blade.php
+++ resources/views/2016/partials/clipping.blade.php
@@ -8,15 +8,27 @@
                     @foreach($clipping as $item)
                         <article class="white-panel"><a href="{{ $item['pdf'] }}"><img src="{{ $item['jpg'] }}" alt=""></a>
                             <h4><a href="{{ $item['pdf'] }}">{{ $item['title'] }}</a></h4>
-                            <p>{{ $item['lead'] }}</p>
+                            <p class="lead1">{{ $item['lead'] }}</p>
+
+
+
+                            <div class="col-xs-12 borda-preta-acima detalhes">
+                                <div class="pull-left">
+                                    <p class="vehicle">{{ $item['vehicle'] }}</p>
+                                </div>
+                                <div class="pull-right">
+                                    <a class="vermais" href="{{ $item['pdf'] }}"> <i class="fa fa-plus" aria-hidden="true"></i></a>
+                                </div>
+                            </div>
+
                         </article>
                     @endforeach
                 </section>
             </div>
 
-            <div class="row text-center">
+            {{--<div class="row text-center">
                 <button type="button" class="btn btn-default">Ver mais clippings</button>
-            </div>
+            </div>--}}
         </div>
     </section>
 @endif
