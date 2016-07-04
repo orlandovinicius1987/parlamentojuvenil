e2a062922378f68c938ea9a39f949bbaac7e427e
Breno Trengrouse <brenot@gmail.com>
2016-07-04 19:53:37 +0000

    Depoimentos

--- resources/views/2016/partials/projects.blade.php
+++ resources/views/2016/partials/projects.blade.php
@@ -1,39 +1,4 @@
-{{--
-<section id="twitter"  class="fundo-azul">
-    <div id="twitter-feed" class="carousel slide" data-interval="false">
-        <div class="row">
-            <div class="col-sm-6 col-sm-offset-3">
-                <div class="text-center carousel-inner center-block quadro-amarelo borda-preta">
-
-                    <?php $active = 'active'; ?>
-
-                    @foreach ($carrousel as $congressman)
-
-                        <div class="item {{ $active }}">
-                            <img class="foto" src="{!! make_image_url($congressman['photo']) !!}" alt="">
-                            <p class="nome">{{ $congressman['name'] }}</p>
-                            <div class="titulo-projeto">Projeto de Lei Nº 51212/2016 </div>
-                            <div class="corpo-projeto">
-                                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Maecenas congue tellus felis, ut semper nulla ultricies fermentum. Morbi aliquam mi id posuere elementum. Donec fermentum vel augue vel congue. Aenean at lorem a odio mattis tempor. Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Sed venenatis ut mi pretium ultricies. Morbi non metus accumsan, condimentum odio at, euismod libero. Vivamus ut fermentum velit. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus.</p>
-
-                                <p>Nam elementum odio et justo tempor rhoncus. Phasellus malesuada, tortor vitae faucibus cursus, est neque semper arcu, non suscipit nunc magna sed quam. Curabitur vel auctor tellus. Donec rutrum mollis lacinia. Phasellus vel arcu id ante sollicitudin tincidunt. Curabitur porta diam augue. Fusce et porta libero, id rhoncus sapien.</p>
-                            </div>
-                        </div>
-
-                        <?php $active = ''; ?>
-                    @endforeach
-                </div>
-                <a class="twitter-control-left" href="#twitter-feed" data-slide="prev"><i class="fa fa-angle-left"></i></a>
-                <a class="twitter-control-right" href="#twitter-feed" data-slide="next"><i class="fa fa-angle-right"></i></a>
-            </div>
-        </div>
-    </div>
-</section>--}}
-
-
-
 <section id="projects2" class="fundo-azul">
-
     <div class="container">
         <div class='row text-center'>
             <h2 class="">Projetos de Lei</h2>
@@ -130,15 +95,4 @@
             </div>
         </div>
     </div>
-
 </section>
-
-
-
-
-
-
-
-
-
-
