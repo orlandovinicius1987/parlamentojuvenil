eabe50e3f28ba3e7b90b1b4590c923e9dc72a580
brenot <brenot@gmail.com>
2016-08-23 21:07:55 +0000

    Training - Video

---
+++ resources/views/2016/training/video.blade.php
@@ -0,0 +1,38 @@
+@extends('2016.layouts.layout')
+
+@section('contents')
+    @include('2016.partials.header-fixed')
+
+    <section id="header-capacitacao" class="fundo-capacitacao-video">
+        <div class="container">
+            <div class="row">
+                <div class="col-xs12 text-center">
+                    &nbsp;<BR>
+                    <h1>Capacitação</h1>
+                    &nbsp;<BR> &nbsp;<BR>
+                    {{--<div class="capacitacao-heading">Lorem ipsum dolor sit amet, consectetur adipiscing elit. In et tellus eget diam sagittis rutrum at at eros. Vestibulum vel efficitur nibh. Vestibulum consectetur, tortor sed feugiat rhoncus, nisi tortor semper risus, ut accumsan massa neque quis diam. In egestas eros et enim tempus, ac semper enim dignissim. Cras porta elit quis iaculis bibendum. Praesent pellentesque sapien odio, id aliquam metus dictum in. Maecenas dapibus lorem feugiat risus ultrices sollicitudin. Vivamus fringilla ullamcorper libero. Donec eget eleifend ligula. Etiam a consequat urna.</div>--}}
+                </div>
+            </div>
+
+            <div class="container">
+                <div class="row">
+                    <div class="col-xs12">
+                        <div class='embed-container'><iframe src='https://www.youtube.com/embed/Jv-8FS-prn4' frameborder='0' allowfullscreen></iframe></div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </section>
+
+{{--    <section class="fundo-azul1">
+        <div class="container">
+            <div class="row">
+                <div class="col-xs12">
+
+                </div>
+            </div>
+        </div>
+    </section>--}}
+
+
+@stop
