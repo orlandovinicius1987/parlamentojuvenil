44e50a6e6f0115394cafca3af90de79b6013dc8c
Antonio Carlos Ribeiro <acr@antoniocarlosribeiro.com>
2016-09-29 16:02:49 +0000

    Add modal to quiz

--- resources/views/scripts/vueQuiz.blade.php
+++ resources/views/scripts/vueQuiz.blade.php
@@ -1,4 +1,48 @@
+<!-- template for the modal component -->
+<script type="x/template" id="modal-template">
+    <div class="modal-mask" v-show="show" transition="modal">
+        <div class="modal-wrapper">
+            <div class="modal-container">
+
+                <div class="modal-header">
+                    <slot name="header">
+                        default header
+                    </slot>
+                </div>
+
+                <div class="modal-body">
+                    <slot name="body">
+                        default body
+                    </slot>
+                </div>
+
+                <div class="modal-footer">
+                    <slot name="footer">
+                        default footer
+                        <button class="modal-default-button"
+                        @click="show = false">
+                        OK
+                        </button>
+                    </slot>
+                </div>
+            </div>
+        </div>
+    </div>
+</script>
+
 <script>
+    // register modal component
+    Vue.component('modal', {
+        template: '#modal-template',
+        props: {
+            show: {
+                type: Boolean,
+                required: true,
+                twoWay: true
+            }
+        }
+    })
+
     if (jQuery("#vue-quiz").length)
     {
         var vueQuiz = new Vue({
@@ -8,16 +52,30 @@
                 id: '{{ $itemId }}',
                 baseUrl: '{{ route('quiz.index', ['year' => 2016]) }}',
                 currentQuestion: 0,
-                questions: [],
+                quiz: null,
+                askForConfirmation: false,
             },
 
             methods: {
                 loadQuestions: function () {
-                    this.$http.get(this.baseUrl+'/'+this.id+'/questions', function (questions)
+                    this.$http.get(this.baseUrl+'/'+this.id+'/questions', function (quiz)
                     {
-                        this.questions = questions;
+                        this.quiz = quiz;
                     });
-                }
+                },
+
+                answerQuestion: function (answer) {
+                    this.askForConfirmation = true;
+                },
+
+                sendAnsweredQuestion: function (answer) {
+                    this.$http.get(this.baseUrl+'/'+this.id+'/answer/'+this.currentQuestion+'/'+answer, function (result)
+                    {
+
+                    });
+
+                    this.currentQuestion++;
+                },
             },
             
             ready: function() {
@@ -25,4 +83,7 @@
             }
         });
     }
+
 </script>
+
+
