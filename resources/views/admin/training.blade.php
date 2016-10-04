de7063e13813a60703d9e61e5ba082e34bbbe86f
Antonio Carlos Ribeiro <acr@antoniocarlosribeiro.com>
2016-10-04 19:16:30 +0000

    Change title

--- resources/views/admin/training.blade.php
+++ resources/views/admin/training.blade.php
@@ -12,7 +12,7 @@
             <div class="col-lg-12">
                 <div class="panel panel-default">
                     <div class="panel-heading">
-                        Listagem
+                        Listagem dos itens de capacitação vistos/respondidos pelo parlamentar juvenil
                     </div>
                     <!-- /.panel-heading -->
                     <div class="panel-body">
