b18a9742ccdd904177e0e8fd9ba19a0577bfbd87
Antonio Carlos Ribeiro <acr@antoniocarlosribeiro.com>
2017-04-06 11:20:08 -0300

    Add logged user class

--- app/Services/SocialLogin/LoggedUser.php
+++ app/Services/SocialLogin/LoggedUser.php
@@ -2,135 +2,57 @@
 
 namespace App\Services\SocialLogin;
 
-use Auth;
-use App\User;
-use App\SocialNetwork;
-use League\Flysystem\Exception;
-use Illuminate\Support\Facades\Hash;
-use App\Repositories\UsersRepository;
-use App\Repositories\SocialUserRepository;
-
-class SocialUserService
+class LoggedUser
 {
-    private $socialUserRepository;
+    public $user;
 
-    private $usersRepository;
+    public $student;
 
-    public function __construct(SocialUserRepository $socialUserRepository, UsersRepository $usersRepository)
+    public $socialUser;
+
+    public $socialNetwork;
+
+    public $socialNetworkUser;
+
+    /**
+     * @param mixed $socialNetwork
+     */
+    public function setSocialNetwork($socialNetwork)
     {
-        $this->socialUserRepository = $socialUserRepository;
-        $this->usersRepository = $usersRepository;
-    }
-
-    public function findOrCreate($socialNetwork, $socialUserPlatform)
-    {
-        $email = $this->getEmail($socialUserPlatform, $socialNetwork);
-
-        session(['email' => $email]);
-
-        $socialNetwork = $this->getSocialNetwork($socialNetwork);
-
-        $socialUser = $this->findOrCreateSocialUser($socialNetwork, $socialUserPlatform);
-
-        return $socialUser;
+        $this->socialNetwork = $socialNetwork;
     }
 
     /**
-     * @param $email
+     * @param mixed $socialNetworkUser
      */
-    private function findOrCreateUserByEmail($email)
+    public function setSocialNetworkUser($socialNetworkUser)
     {
-        if (! is_null($user = User::where('email', $email)->first())) {
-            return $user;
-        }
-
-        return User::create([
-            'email' => $email,
-            'password' => Hash::make(str_random(128)),
-        ]);
+        $this->socialNetworkUser = $socialNetworkUser;
     }
 
     /**
-     * @param $socialUser
-     *
-     * @return \Illuminate\Http\RedirectResponse
+     * @param mixed $socialUser
      */
-    public function login($user)
+    public function setSocialUser($socialUser)
     {
-        return auth()->login($user);
+        $this->socialUser = $socialUser;
+
+        $this->setStudent($socialUser->student);
     }
 
-    private function getEmail($socialUserPlatform, $socialNetwork)
+    /**
+     * @param mixed $student
+     */
+    public function setStudent($student)
     {
-        return $socialUserPlatform->getEmail() ?: sprintf('%s@%s.legislaqui.rj.gov.br', $socialUserPlatform->getId(), $socialNetwork);
+        $this->student = $student;
     }
 
-    public function findOrCreateUser($socialUser, $email)
+    /**
+     * @param mixed $user
+     */
+    public function setUser($user)
     {
-        if (!$user = $this->socialUserRepository->findBySocialNetworkId($socialUser->id) ){
-
-                $user = $this->socialUserRepository->createUser($email, $socialUser);
-                return $user;
-        }
-        return $user;
+        $this->user = $user;
     }
-
-    public function getSocialNetwork($socialNetwork)
-    {
-        if (is_null($model = SocialNetwork::where('name', $socialNetwork)->first())) {
-            throw new Exception('Social network not found: '.$socialNetwork);
-        }
-
-        return $model;
-    }
-
-    public function findOrCreateSocialUser($socialNetwork, $socialUserPlatform)
-    {
-         if(! $socialUser = $this->socialUserRepository->findBySocialNetworkUserId($socialUserPlatform->id))
-         {
-              return $this->socialUserRepository->createSocialUser($socialNetwork, $socialUserPlatform);
-         }
-
-        return $socialUser;
-    }
-
-    public function loginSocialUser($studentId, $socialUserId, $email, $socialUserPlatform)
-    {
-        $socialUser = $this->findOrCreateUserByStudent(
-            $studentId,
-            $socialUserId,
-            $email,
-            $socialUserPlatform
-        );
-
-        $socialUser->user->socialUser = $socialUser;
-
-        Auth::login($socialUser->user);
-    }
-
-    public function findOrCreateUserByStudent($studentId, $socialUserId, $email, $socialUserPlatform)
-    {
-        $socialUser = $this->socialUserRepository->findBySocialNetworkUserId($socialUserPlatform->getId());
-
-        if (is_null($socialUser->student)) {
-            $socialUser->student_id = $studentId;
-        }
-
-        if (is_null($socialUser->user)) {
-            $socialUserByStudent = $this->socialUserRepository->findOtherSocialUsersByStudentId($studentId, $socialUserPlatform->getId());
-
-            if ($socialUserByStudent->count() == 0) {
-                $user = $this->findOrCreateUserByEmail($email);
-            } else {
-                $user = $socialUserByStudent->user;
-            }
-
-            $socialUser->user_id = $user->id;
-        }
-
-        $socialUser->save();
-
-        return $socialUser;
-    }
-
 }
