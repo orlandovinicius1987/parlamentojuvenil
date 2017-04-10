919c412adeae8c64706751edbaf585ae4bffe76e
vagrant <vagrant@homestead>
2017-04-10 17:55:49 +0000

    404 page -extends

--- resources/views/errors/404.blade.php
+++ resources/views/errors/404.blade.php
@@ -1,108 +1,19 @@
-<!DOCTYPE html>
-<html>
+@extends('2017.layouts.layout')
 
-<head>
-    <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta name="description" content="">
-    <meta name="author" content="">
-    <title>Parlamento Juvenil - ALERJ</title>
+@section('contents')
 
-    <!-- javascript -->
-    <script src="//code.jquery.com/jquery-1.9.1.min.js"></script>
-
-    <!-- Datepicker -->
-    <link rel="stylesheet" type="text/css" href="//cdnjs.cloudflare.com/ajax/libs/bootstrap-datepicker/1.3.1/css/datepicker3.css">
-
-    <!-- Font Awesome -->
-    <link rel="stylesheet" href="//maxcdn.bootstrapcdn.com/font-awesome/4.6.3/css/font-awesome.min.css">
-
-    <!-- Google Fonts -->
-    <link href="//fonts.googleapis.com/css?family=Amatic+SC:400,700%7CShadows+Into+Light%7CDosis:400,200,600" rel="stylesheet" type="text/css" >
-    <link href="//fonts.googleapis.com/css?family=Lato:100" rel="stylesheet" type="text/css">
-    <link href='//fonts.googleapis.com/css?family=Lato:300,400,700,400italic,300italic' rel='stylesheet' type='text/css'>
-    <link href='//fonts.googleapis.com/css?family=Kreon:400,300|Palanquin+Dark' rel='stylesheet' type='text/css'>
-
-    <!-- Blueimp Lightbox -->
-    <link rel="stylesheet" href="//blueimp.github.io/Gallery/css/blueimp-gallery.min.css">
-
-
-    <link href="/pj2016/css/bootstrap.min.css" rel="stylesheet">
-    <link href="/pj2016/css/font-awesome.min.css" rel="stylesheet">
-    <link href="/pj2016/css/main.css" rel="stylesheet">
-    <link href="/pj2016/css/animate.css" rel="stylesheet">
-    <link href="/pj2016/css/responsive.css" rel="stylesheet">
-
-
-    <link href="https://fonts.googleapis.com/css?family=Open+Sans" rel="stylesheet">
-
-        <style>
-            html, body {
-                height: 100%;
-            }
-
-            body {
-                margin: 0;
-                padding: 0;
-                width: 100%;
-                color: #B0BEC5;
-                display: table;
-                font-weight: 100;
-                font-family: 'Open Sans', serif;
-            }
-
-            .title {
-                font-size: 72px;
-                margin-bottom: 40px;
-            }
-
-
-
-            .error404-box {
-                width: 370px;
-                margin: 7% auto;
-            }
-
-
-            .error404-logo {
-                text-align: center;
-                margin-bottom: 10px;
-                font-weight: 300;
-            }
-
-            .error404-box-msg {
-                    margin: 0;
-                    text-align: center;
-                    padding: 0 20px 0px 20px;
-                font-size: 26px;
-                color: #EE272B;
-                }
-            .error404-box-body {
-                background: #fff;
-                padding: 20px;
-                border-top: 0;
-                color: #848484;
-                text-align: center;
-                font-size: 24px;
-            }
-
-        </style>
-
-</head>
-<body>
     <section id="error" class="fundo-azul">
         <div class="container">
             <div class="error404-box">
                 <div class="error404-logo">
-                   <img src="/templates/2017/assets/img/404error.jpg" alt="404 Error">
+                    <img src="/templates/2017/assets/img/404error.jpg" alt="404 Error">
                 </div>
                 <div class="error404-box-body">
                     <p class="error404-box-msg">Página não encontrada</p>
                     erro 404
                 </div>
             </div>
-
         </div>
     </section>
-</body>
-</html>
+    @stop
+
