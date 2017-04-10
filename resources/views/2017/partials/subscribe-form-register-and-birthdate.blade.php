45990cb9c1adc67bde65be93da864845a9cb0583
vagrant <vagrant@homestead>
2017-04-10 17:42:21 +0000

    Form Subscribe Style

--- resources/views/2017/partials/subscribe-form-register-and-birthdate.blade.php
+++ resources/views/2017/partials/subscribe-form-register-and-birthdate.blade.php
@@ -3,47 +3,88 @@
 @section('contents')
     {{--@include('2016.partials.header-fixed')--}}
 
-    <section id="titulo" class="fundo-inscricao">
-        <div class="container">
-            <div class="row">
-                <div class="col-xs12 text-center">
-                    <h1>&nbsp;</h1>
-                    <h1>Inscreva-se</h1>
-                </div>
+    <section id="subscribe" class="fundo-inscricao">
+        <div class="row">
+            <div class="col-xs-12 text-center">
+                <h1 class="subscribe-title">Insira os dados para a sua identificação:</h1>
             </div>
         </div>
+        <div class="row form-subscribe">
+
+            <div class="col-xs-offset-2 col-xs-4 text-center">
+                <form id="login" method="POST" action="{{ route('auth.student.login') }}">
+                    {!! csrf_field() !!}
+                    <input
+                            v-model="name"
+                            type="text"
+                            class="form-control input-lg"
+                            value="{{ Input::old('name') }}"
+                            placeholder="Digite sua matrícula"
+                            name="registration"
+                            id="registration"
+                            required data-validation-required-message="Por favor digite seu nome."
+                    >
+            </div>
+            <div class="col-xs-4 text-center">
+                <input
+                        v-model="birthdate"
+                        type="text"
+                        class="form-control input-lg"
+                        value="{{ Input::old('birthdate') }}"
+                        placeholder="Data de nascimento"
+                        name="birthdate"
+                        id="birthdate"
+                        required
+                        data-validation-required-message="Por favor digite sua data de nascimento."
+                >
+            </div>
+
+        </div>
+
+        <div class="row">
+            <div class="col-xs-offset-5 col-xs-2 text-center">
+                <button id="submit" type="submit" class="btn btn-lg btn-primary btn-block btn-submit-subscription">
+                    Enviar
+                </button>
+                </form>
+            </div>
+        </div>
+
+
+{{--
+        <form id="login" method="POST" action="{{ route('auth.student.login') }}">
+            {!! csrf_field() !!}
+            <input
+                    v-model="name"
+                    type="text"
+                    class="form-control input-lg"
+                    value="{{ Input::old('name') }}"
+                    placeholder="Digite sua matrícula"
+                    name="registration"
+                    id="registration"
+                    required data-validation-required-message="Por favor digite seu nome."
+            >
+            <input
+                    v-model="birthdate"
+                    type="text"
+                    class="form-control input-lg"
+                    value="{{ Input::old('birthdate') }}"
+                    placeholder="Data de nascimento"
+                    name="birthdate"
+                    id="birthdate"
+                    required
+                    data-validation-required-message="Por favor digite sua data de nascimento."
+            >
+            <button id="submit" type="submit" class="btn btn-lg btn-primary btn-block btn-submit-subscription">
+                Enviar
+            </button>
+        </form>
+--}}
+
+
     </section>
 
-    <form id="login" method="POST" action="{{ route('auth.student.login') }}">
-        {!! csrf_field() !!}
 
-        <input
-                v-model="name"
-                type="text"
-                class="form-control input-lg"
-                value="{{ Input::old('name') }}"
-                placeholder="Digite sua matrícula"
-                name="registration"
-                id="registration"
-                required data-validation-required-message="Por favor digite seu nome."
-        >
 
-        <input
-                v-model="birthdate"
-                type="text"
-                class="form-control input-lg"
-                value="{{ Input::old('birthdate') }}"
-                placeholder="Data de nascimento"
-                name="birthdate"
-                id="birthdate"
-                required
-                data-validation-required-message="Por favor digite sua data de nascimento."
-        >
-
-        <button id="submit" type="submit" class="btn btn-lg btn-primary btn-block btn-submit-subscription">
-            Enviar
-        </button>
-
-    </form>
 @stop
 
