77e8169d9b56ffecd0735736e3417f3e99d4be66
Antonio Carlos Ribeiro <acr@antoniocarlosribeiro.com>
2017-04-10 11:49:57 -0300

    Fix social login

--- app/Http/Controllers/SocialAuthController.php
+++ app/Http/Controllers/SocialAuthController.php
@@ -3,7 +3,6 @@
 namespace App\Http\Controllers;
 
 use App\Services\SocialLogin\SocialUserService;
-use Socialite;
 use DB;
 
 class SocialAuthController extends Controller
@@ -15,18 +14,9 @@ class SocialAuthController extends Controller
         $this->socialUserService = $socialUserService;
     }
 
-    private function isSocialNetworkIsLoggedIn($socialNetwork)
-    {
-        if (loggedUser()->logged()) {
-            return false;
-        }
-
-        return loggedUser()->socialNetwork == $socialNetwork;
-    }
-
     public function login($socialNetwork)
     {
-        return $this->getDriver($socialNetwork)->redirect();
+        return $this->socialUserService->getDriver($socialNetwork)->redirect();
     }
 
     public function socialNetworkCallback($socialNetwork)
@@ -34,47 +24,5 @@ class SocialAuthController extends Controller
         $this->socialUserService->socialNetworkLogin($socialNetwork);
 
         return view('2017.partials.subscribe-form-register-and-birthdate');
-    }
-
-    public function getDriver($driver)
-    {
-        return Socialite::driver($driver);
-    }
-
-    /**
-     * @param $socialNetwork
-     */
-    public function socialNetworkLogin($socialNetwork)
-    {
-        if (! $this->isSocialNetworkIsLoggedIn($socialNetwork)) {
-            $socialNetworkUser = $this->socialUserService->makeSocialNetworkUser($this->getDriver($socialNetwork)->user());
-
-            $socialUser = $this->socialUserService->findOrCreate($socialNetwork, $socialNetworkUser);
-
-            $this->storeUserInSession(
-                $socialNetwork,
-                $socialUser,
-                $socialNetworkUser,
-                $this->getEmail($socialNetworkUser, $socialNetwork)
-            );
-        }
-    }
-
-    private function getEmail($socialNetworkUser, $socialNetwork)
-    {
-        return $socialNetworkUser->getEmail() ?: sprintf('%s@%s.parlamentojuvenil.rj.gov.br', $socialNetworkUser->getId(), $socialNetwork);
-    }
-
-    /**
-     * @param $socialNetwork
-     * @param $socialUser
-     * @param $socialNetworkUser
-     */
-    private function storeUserInSession($socialNetwork, $socialUser, $socialNetworkUser, $email)
-    {
-        loggedUser()->setSocialNetwork($socialNetwork)
-                    ->setSocialUser($socialUser)
-                    ->setSocialNetworkUser($socialNetworkUser)
-                    ->setEmail($email);
     }
 }
