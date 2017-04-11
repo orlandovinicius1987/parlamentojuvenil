397e7b6a6f3a15b0904fee7cf47f6ff45ebc659c
vagrant <vagrant@homestead>
2017-04-11 18:14:18 +0000

    Botoes Novos - Login Social 2

--- resources/views/2017/auth/index.blade.php
+++ resources/views/2017/auth/index.blade.php
@@ -9,47 +9,39 @@
                     <div>
                         <h3 class="identify-title">Identifique-se</h3>
                     </div>
-
-                        <div class="social-login">
-
-                                <a href="{{ route('auth.social.redirect', ['facebook']) }}" class="facebook-login">
-                                    <i class="fa fa-facebook fa-lg"></i>
-                                    Entrar com facebook
-                                </a>
-                                <a href="{{ route('auth.social.redirect', ['twitter']) }}"  class="twitter-login" >
-                                    <i class="fa fa-twitter fa-lg"></i>
-                                    Entrar com Twitter
-                                </a>
-                                <a href="{{ route('auth.social.redirect', ['youtube']) }}"  class="instagram-login">
-                                    <i class="fa fa-instagram fa-lg"></i>
-                                    Entrar com Instagram
-                                </a>
-                                <a href="{{ route('auth.social.redirect', ['linkedin']) }}"  class="linkedin-login">
-                                    <i class="fa fa-linkedin fa-lg"></i>
-                                    Entrar com Linkedin
-                                </a>
-                                <a href="{{ route('auth.social.redirect', ['instagram']) }}" class="youtube-login" >
-                                    <i class="fa fa-youtube fa-lg"></i>
-                                    Entrar com Youtube
-                                </a>
-
-                        </div>
-
-
-{{--
-                            <a class="social-login-btn social-facebook" href="{{ route('auth.social.redirect', ['facebook']) }}"><img class="img-responsive" src="/templates/2017/assets/img/socialbtn-facebook.png"></a>
-                            <a class="social-login-btn social-twitter" href="{{ route('auth.social.redirect', ['twitter']) }}"><img class="img-responsive" src="/templates/2017/assets/img/socialbtn-twitter.png"></a>
-                            <a class="social-login-btn social-instagram" href="{{ route('auth.social.redirect', ['youtube']) }}"><img class="img-responsive" src="/templates/2017/assets/img/socialbtn-instagram.png"></a>
-                            <a class="social-login-btn social-linkedin" href="{{ route('auth.social.redirect', ['linkedin']) }}"><img class="img-responsive" src="/templates/2017/assets/img/socialbtn-linkedin.png"></a>
-                            <a class="social-login-btn social-youtube" href="{{ route('auth.social.redirect', ['instagram']) }}"><img class="img-responsive" src="/templates/2017/assets/img/socialbtn-youtube.png"></a>
-                    --}}
-
+                    <div class="social-login">
+                        <a href="{{ route('auth.social.redirect', ['facebook']) }}" class="facebook-login">
+                            <i class="fa fa-facebook fa-lg"></i>
+                            Entrar com facebook
+                        </a>
+                        <a href="{{ route('auth.social.redirect', ['twitter']) }}"  class="twitter-login" >
+                            <i class="fa fa-twitter fa-lg"></i>
+                            Entrar com Twitter
+                        </a>
+                        <a href="{{ route('auth.social.redirect', ['youtube']) }}"  class="instagram-login">
+                            <i class="fa fa-instagram fa-lg"></i>
+                            Entrar com Instagram
+                        </a>
+                        <a href="{{ route('auth.social.redirect', ['linkedin']) }}"  class="linkedin-login">
+                            <i class="fa fa-linkedin fa-lg"></i>
+                            Entrar com Linkedin
+                        </a>
+                        <a href="{{ route('auth.social.redirect', ['instagram']) }}" class="youtube-login" >
+                            <i class="fa fa-youtube fa-lg"></i>
+                            Entrar com Youtube
+                        </a>
+                    </div>
+                    {{--
+                                                <a class="social-login-btn social-facebook" href="{{ route('auth.social.redirect', ['facebook']) }}"><img class="img-responsive" src="/templates/2017/assets/img/socialbtn-facebook.png"></a>
+                                                <a class="social-login-btn social-twitter" href="{{ route('auth.social.redirect', ['twitter']) }}"><img class="img-responsive" src="/templates/2017/assets/img/socialbtn-twitter.png"></a>
+                                                <a class="social-login-btn social-instagram" href="{{ route('auth.social.redirect', ['youtube']) }}"><img class="img-responsive" src="/templates/2017/assets/img/socialbtn-instagram.png"></a>
+                                                <a class="social-login-btn social-linkedin" href="{{ route('auth.social.redirect', ['linkedin']) }}"><img class="img-responsive" src="/templates/2017/assets/img/socialbtn-linkedin.png"></a>
+                                                <a class="social-login-btn social-youtube" href="{{ route('auth.social.redirect', ['instagram']) }}"><img class="img-responsive" src="/templates/2017/assets/img/socialbtn-youtube.png"></a>
+                                        --}}
                 </div>
             </div>
-
             <div class="row">
                 <div class="col-md-offset-2 col-md-8">
-
                     <div class="nao-possui-redes">
                         <a href="naotenhorede.html"><p><a href="{{ route('auth.login.email') }}">Caso você não esteja em nenhuma dessas redes sociais, clique aqui.</a></p></a>
                     </div>
@@ -57,6 +49,4 @@
             </div>
         </div>
     </section>
-
-
 @stop
