7c60f066d67d1f9e76b855374103a7b1e99d083a
Antonio Carlos Ribeiro <acr@antoniocarlosribeiro.com>
2017-04-18 23:23:22 -0300

    Add view

--- resources/views/2017/subscriptions/success.blade.php
+++ resources/views/2017/subscriptions/success.blade.php
@@ -2,6 +2,6 @@
 
 @section('contents')
     <h2>Sua inscrição no Parlamento Juvenil {{ config('app.year') }} foi realizada!</h2>
-    
+
     <h1>Por favor aguarde o contato da equipe do Parlamento Juvenil.</h1>
 @stop
