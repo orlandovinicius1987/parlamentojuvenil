e10ae2e311e00178acae5c1887296082bb0fcbe2
Antonio Carlos Ribeiro <acr@antoniocarlosribeiro.com>
2017-04-26 14:39:43 -0300

    Add social profiles

--- resources/views/admin/subscriptions/edit.blade.php
+++ resources/views/admin/subscriptions/edit.blade.php
@@ -3,7 +3,7 @@
 @section('container')
     <div>
         <div class="row">
-            <div class="col-lg-12">
+            <div class="col-lg-11">
                 <h1 class="page-header">{{ $student->name }}</h1>
             </div>
         </div>
@@ -17,50 +17,101 @@
                     <div class="panel-body">
                         <div class="row">
                             <div class="col-lg-12">
-                                @foreach($student->socialUsers as $socialUser)
-                                    <a href="{{ $socialUser->profileUrl }}">
-                                        <img
-                                            src="/templates/{{ get_current_year() }}/assets/img/socialbtn-{{ $socialUser->socialNetwork->slug }}.png"
-                                            alt=""
-                                            width="25px"
-                                        >
-                                    </a>
-                                @endforeach
+                                <div class="row">
+                                    @foreach($student->socialUsers as $socialUser)
+                                        <div class="col-xs-3 well">
+                                            <a href="{{ $socialUser->profileUrl }}">
+                                                <div class="row">
+                                                    <div class="col-xs-5">
+                                                        <img
+                                                            src="{{ $socialUser->user->avatar }}"
+                                                            alt=""
+                                                            width="80px"
+                                                        >
+                                                    </div>
+
+                                                    <div class="col-xs-2">
+                                                        <h1 class="text-center">
+                                                            <i class="fa fa-arrow-right"></i>
+                                                        </h1>
+                                                    </div>
+
+                                                    <div class="col-xs-5">
+                                                        <img
+                                                                src="/templates/{{ get_current_year() }}/assets/img/socialbtn-{{ $socialUser->socialNetwork->slug }}.png"
+                                                                alt=""
+                                                                width="80px"
+                                                        >
+                                                    </div>
+                                                </div>
+                                            </a>
+                                        </div>
+
+                                        <div class="col-xs-3 well">
+                                            <a href="{{ $socialUser->profileUrl }}">
+                                                <div class="row">
+                                                    <div class="col-xs-5">
+                                                        <img
+                                                                src="{{ $socialUser->user->avatar }}"
+                                                                alt=""
+                                                                width="80px"
+                                                        >
+                                                    </div>
+
+                                                    <div class="col-xs-2">
+                                                        <h1 class="text-center">
+                                                            <i class="fa fa-arrow-right"></i>
+                                                        </h1>
+                                                    </div>
+
+                                                    <div class="col-xs-5">
+                                                        <img
+                                                                src="/templates/{{ get_current_year() }}/assets/img/socialbtn-{{ $socialUser->socialNetwork->slug }}.png"
+                                                                alt=""
+                                                                width="80px"
+                                                        >
+                                                    </div>
+                                                </div>
+                                            </a>
+                                        </div>
+                                    @endforeach
+                                </div>
                             </div>
                         </div>
-                        <!-- /.row (nested) -->
                     </div>
-                    <!-- /.panel-body -->
+                    <!-- /.row (nested) -->
                 </div>
-                <!-- /.panel -->
+                <!-- /.panel-body -->
             </div>
+            <!-- /.panel -->
         </div>
+    </div>
 
-        <div class="row">
-            <div class="col-lg-12">
-                <div class="panel panel-default">
-                    <div class="panel-heading">
-                        Editar dados da inscrição
-                    </div>
-                    <div class="panel-body">
-                        <div class="row">
-                            <div class="col-lg-12">
-                                <form method="POST" id="vue-subscribe" action="{{ route('subscriptions.edit', ['id' => $subscription->id])}}">
-                                    {!! csrf_field() !!}
+    <div class="row">
+        <div class="col-lg-12">
+            <div class="panel panel-default">
+                <div class="panel-heading">
+                    Editar dados da inscrição
+                </div>
+                <div class="panel-body">
+                    <div class="row">
+                        <div class="col-lg-12">
+                            <form method="POST" id="vue-subscribe" action="{{ route('subscriptions.edit', ['id' => $subscription->id])}}">
+                                {!! csrf_field() !!}
 
-                                    @include('partials.subscribe-form-fields')
+                                @include('partials.subscribe-form-fields')
 
-                                    {{--<pre>@{{ $data | json }}</pre>--}}
-                                </form>
-                            </div>
+                                {{--<pre>@{{ $data | json }}</pre>--}}
+                            </form>
                         </div>
-                        <!-- /.row (nested) -->
                     </div>
-                    <!-- /.panel-body -->
+                    <!-- /.row (nested) -->
                 </div>
-                <!-- /.panel -->
+                <!-- /.panel-body -->
             </div>
+            <!-- /.panel -->
         </div>
+    </div>
     </div>
 @stop
 
