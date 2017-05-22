416994cd3c42ac8df7202ac54eb95ad774242010
Breno Trengrouse <brenot@gmail.com>
2017-05-22 17:41:20 +0000

    Vote- new style

--- resources/views/2017/vote/index.blade.php
+++ resources/views/2017/vote/index.blade.php
@@ -11,54 +11,17 @@
         </div>
 
         <div class="row vote">
-            <div class="col-sm-6">
-                <div class="well amarelo1">
-                    <div class="col-xs-12 col-sm-5 foto-candidato">
-                        <img src="/templates/2017/images/foto_candidato.jpg" class="img-responsive">
-                    </div>
-                    <div class="col-xs-12 col-sm-7 dados-candidato">
-                        <p>Candidato(a)</p>
-                        <p class="nome-candidato">Fulano de Tal</p>
-                        <p class="cargo">Deputado(a) Estadual Juvenil</p>
-                        <p class="escola">Escola Ipsem Lorem</p>
-                        <a href="" class="btn btn-primary btn-large center-block">VOTAR</a>
-                    </div>
-                </div>
-            </div>
-            <div class="col-sm-6">
-                <div class="well lilas1">
-                    <div class="col-xs-12 col-sm-5 foto-candidato">
-                        <img src="/templates/2017/images/foto_candidato.jpg" class="img-responsive">
-                    </div>
-                    <div class="col-xs-12 col-sm-7 dados-candidato">
-                        <p>Candidato(a)</p>
-                        <p class="nome-candidato">Fulano de Tal</p>
-                        <p class="cargo">Deputado(a) Estadual Juvenil</p>
-                        <p class="escola">Escola Ipsem Lorem</p>
-                        <a href="" class="btn btn-primary btn-large center-block">VOTAR</a>
-                    </div>
-                </div>
-            </div>
-            <div class="col-sm-6">
-                <div class="well laranja1">
-                    <div class="col-xs-12 col-sm-5 foto-candidato">
-                        <img src="/templates/2017/images/foto_candidato.jpg" class="img-responsive">
-                    </div>
-                    <div class="col-xs-12 col-sm-7 dados-candidato">
-                        <p>Candidato(a)</p>
-                        <p class="nome-candidato">Fulano de Tal</p>
-                        <p class="cargo">Deputado(a) Estadual Juvenil</p>
-                        <p class="escola">Escola Ipsem Lorem</p>
-                        <a href="" class="btn btn-primary btn-large center-block">VOTAR</a>
-                    </div>
-                </div>
-            </div>
-            <div class="col-sm-6">
+
+            <div class="col-sm-4">
                 <div class="well verde1">
-                    <div class="col-xs-12 col-sm-5 foto-candidato">
-                        <img src="/templates/2017/images/foto_candidato.jpg" class="img-responsive">
+                    <div class="col-xs-12 col-sm-offset-3 col-sm-6 text-center">
+                        <div class="foto-candidato-outer">
+                            <div class="foto-candidato-inner" >
+                                <img src="https://scontent.xx.fbcdn.net/v/t1.0-1/p100x100/10269379_830166683673586_5709921571223448332_n.jpg?oh=3c830a2909059a2772c83e730f07ef8c&amp;oe=59B09DA1" class="img-responsive foto-candidato-social">
+                            </div>
+                        </div>
                     </div>
-                    <div class="col-xs-12 col-sm-7 dados-candidato">
+                    <div class="col-xs-12 col-sm-12 dados-candidato">
                         <p>Candidato(a)</p>
                         <p class="nome-candidato">Fulano de Tal</p>
                         <p class="cargo">Deputado(a) Estadual Juvenil</p>
@@ -67,6 +30,106 @@
                     </div>
                 </div>
             </div>
+
+            <div class="col-sm-4">
+                <div class="well amarelo1">
+                    <div class="col-xs-12 col-sm-offset-3 col-sm-6 text-center">
+                        <div class="foto-candidato-outer">
+                            <div class="foto-candidato-inner" >
+                                <img src="https://scontent.xx.fbcdn.net/v/t1.0-1/p100x100/10269379_830166683673586_5709921571223448332_n.jpg?oh=3c830a2909059a2772c83e730f07ef8c&amp;oe=59B09DA1" class="img-responsive foto-candidato-social">
+                            </div>
+                        </div>
+                    </div>
+                    <div class="col-xs-12 col-sm-12 dados-candidato">
+                        <p>Candidato(a)</p>
+                        <p class="nome-candidato">Fulano de Tal</p>
+                        <p class="cargo">Deputado(a) Estadual Juvenil</p>
+                        <p class="escola">Escola Ipsem Lorem</p>
+                        <a href="" class="btn btn-primary btn-large center-block">VOTAR</a>
+                    </div>
+                </div>
+            </div>
+
+            <div class="col-sm-4">
+                <div class="well lilas1">
+                    <div class="col-xs-12 col-sm-offset-3 col-sm-6 text-center">
+                        <div class="foto-candidato-outer">
+                            <div class="foto-candidato-inner" >
+                                <img src="https://scontent.xx.fbcdn.net/v/t1.0-1/p100x100/10269379_830166683673586_5709921571223448332_n.jpg?oh=3c830a2909059a2772c83e730f07ef8c&amp;oe=59B09DA1" class="img-responsive foto-candidato-social">
+                            </div>
+                        </div>
+                    </div>
+                    <div class="col-xs-12 col-sm-12 dados-candidato">
+                        <p>Candidato(a)</p>
+                        <p class="nome-candidato">Fulano de Tal</p>
+                        <p class="cargo">Deputado(a) Estadual Juvenil</p>
+                        <p class="escola">Escola Ipsem Lorem</p>
+                        <a href="" class="btn btn-primary btn-large center-block">VOTAR</a>
+                    </div>
+                </div>
+            </div>
+
+            <div class="col-sm-4">
+                <div class="well vermelho1">
+                    <div class="col-xs-12 col-sm-offset-3 col-sm-6 text-center">
+                        <div class="foto-candidato-outer">
+                            <div class="foto-candidato-inner" >
+                                <img src="https://scontent.xx.fbcdn.net/v/t1.0-1/p100x100/10269379_830166683673586_5709921571223448332_n.jpg?oh=3c830a2909059a2772c83e730f07ef8c&amp;oe=59B09DA1" class="img-responsive foto-candidato-social">
+                            </div>
+                        </div>
+                    </div>
+                    <div class="col-xs-12 col-sm-12 dados-candidato">
+                        <p>Candidato(a)</p>
+                        <p class="nome-candidato">Fulano de Tal</p>
+                        <p class="cargo">Deputado(a) Estadual Juvenil</p>
+                        <p class="escola">Escola Ipsem Lorem</p>
+                        <a href="" class="btn btn-primary btn-large center-block">VOTAR</a>
+                    </div>
+                </div>
+            </div>
+
+            <div class="col-sm-4">
+                <div class="well azul1">
+                    <div class="col-xs-12 col-sm-offset-3 col-sm-6 text-center">
+                        <div class="foto-candidato-outer">
+                            <div class="foto-candidato-inner" >
+                                <img src="https://scontent.xx.fbcdn.net/v/t1.0-1/p100x100/10269379_830166683673586_5709921571223448332_n.jpg?oh=3c830a2909059a2772c83e730f07ef8c&amp;oe=59B09DA1" class="img-responsive foto-candidato-social">
+                            </div>
+                        </div>
+                    </div>
+                    <div class="col-xs-12 col-sm-12 dados-candidato">
+                        <p>Candidato(a)</p>
+                        <p class="nome-candidato">Fulano de Tal</p>
+                        <p class="cargo">Deputado(a) Estadual Juvenil</p>
+                        <p class="escola">Escola Ipsem Lorem</p>
+                        <a href="" class="btn btn-primary btn-large center-block">VOTAR</a>
+                    </div>
+                </div>
+            </div>
+
+            <div class="col-sm-4">
+                <div class="well amarelo1">
+                    <div class="col-xs-12 col-sm-offset-3 col-sm-6 text-center">
+                        <div class="foto-candidato-outer">
+                            <div class="foto-candidato-inner" >
+                                <img src="https://scontent.xx.fbcdn.net/v/t1.0-1/p100x100/10269379_830166683673586_5709921571223448332_n.jpg?oh=3c830a2909059a2772c83e730f07ef8c&amp;oe=59B09DA1" class="img-responsive foto-candidato-social">
+                            </div>
+                        </div>
+                    </div>
+                    <div class="col-xs-12 col-sm-12 dados-candidato">
+                        <p>Candidato(a)</p>
+                        <p class="nome-candidato">Fulano de Tal</p>
+                        <p class="cargo">Deputado(a) Estadual Juvenil</p>
+                        <p class="escola">Escola Ipsem Lorem</p>
+                        <a href="" class="btn btn-primary btn-large center-block">VOTAR</a>
+                    </div>
+                </div>
+            </div>
+
+
+
+
         </div>
-    </div>
+   </div>
 @stop
+
