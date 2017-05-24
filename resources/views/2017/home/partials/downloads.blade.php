284eb55c3723fd7edf4a023c02421594409cf8a9
Antonio Carlos Ribeiro <acr@antoniocarlosribeiro.com>
2017-05-24 14:31:08 -0300

    Fix coding style

--- resources/views/2017/home/partials/downloads.blade.php
+++ resources/views/2017/home/partials/downloads.blade.php
@@ -9,26 +9,24 @@
         </div>
 
         <div class="row downloads-links">
-
-                <div class=" col-sm-4">
-                    <a href="/files/apps/parlamentojuvenil/site/2017/downloads/orientacoes-processo-eleitoral-pjrj-2017-1o-turno.pdf" class="well amarelo1">
-                        <i class="fa fa-download" aria-hidden="true"></i>
-                        <p>Orientações para o processo eleitoral - 1º Turno</p>
-                    </a>
-                </div>
-                <div class="col-sm-4">
-                    <a href="/files/apps/parlamentojuvenil/site/2017/downloads/cedulas-votacao-pjrj-2017.pdf" class="well verde01">
-                        <i class="fa fa-download" aria-hidden="true"></i>
-                        <p>Cédulas de Votação -  PJ 2017</p>
-                    </a>
-                </div>
-                <div class="col-sm-4">
-                    <a href="/files/apps/parlamentojuvenil/site/2017/downloads/ata-eleicao-1o-turno-pjrj-2017.doc" class="well azul3">
-                        <i class="fa fa-download" aria-hidden="true"></i>
-                        <p>Ata da Eleição - 1º Turno</p>
-                    </a>
-                </div>
-
+            <div class=" col-sm-4">
+                <a href="/files/apps/parlamentojuvenil/site/2017/downloads/orientacoes-processo-eleitoral-pjrj-2017-1o-turno.pdf" class="well amarelo1">
+                    <i class="fa fa-download" aria-hidden="true"></i>
+                    <p>Orientações para o processo eleitoral - 1º Turno</p>
+                </a>
+            </div>
+            <div class="col-sm-4">
+                <a href="/files/apps/parlamentojuvenil/site/2017/downloads/cedulas-votacao-pjrj-2017.pdf" class="well verde01">
+                    <i class="fa fa-download" aria-hidden="true"></i>
+                    <p>Cédulas de Votação -  PJ 2017</p>
+                </a>
+            </div>
+            <div class="col-sm-4">
+                <a href="/files/apps/parlamentojuvenil/site/2017/downloads/ata-eleicao-1o-turno-pjrj-2017.doc" class="well azul3">
+                    <i class="fa fa-download" aria-hidden="true"></i>
+                    <p>Ata da Eleição - 1º Turno</p>
+                </a>
+            </div>
         </div>
     </div>
 </section>
