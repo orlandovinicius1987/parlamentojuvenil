151ecde33112c8d466bffe8c550bba02c1f62eed
Breno Trengrouse <brenot@gmail.com>
2017-09-28 18:30:53 +0000

    capacitacao - estilo 003

--- resources/views/2017/training/video.blade.php
+++ resources/views/2017/training/video.blade.php
@@ -1,18 +1,57 @@
 @extends('2017.layouts.layout')
 
 @section('contents')
-    <section id="header-capacitacao" class="fundo-capacitacao-video">
+    <section id="header-capacitacao" class="capacitacao-content">
         <div class="container">
             <div class="row">
-                <div class="col-xs12 text-center">
-                    &nbsp;<BR>
-                    <h2>Capacitação</h2><br>
-                    <div class="capacitacao-greatings">Olá <span  class="capacitacao-username">{{ $loggedUser->name }}</span></div>
-                    <div class="capacitacao-tit-video">{{ $lesson['title'] }}</div>
+                <div class="col-sm-2">
+                    <div class="grow grid-item danube-blue">
+                    </div>
+                </div>
+                <div class="col-sm-2">
+                    <div class="grow grid-item verde2">
+                    </div>
+                </div>
+                <div class="col-sm-4">
+                    {{--<div class="row">
+                        <div class="hidden-sm col-sm-hidden col-md-7 col-lg-7">
+                            <div class="grow grid-item--height5 supernova-yellow">
+                            </div>
+                        </div>
+                        <div class="hidden-sm col-md-1 col-lg-1">
+                            <div class="grow grid-item--height5 white">
+                            </div>
+                        </div>
+                        <div class="hidden-sm col-md-3 col-lg-4">
+                            <div class="grow grid-item--height5 lima-green">
+                            </div>
+                        </div>
+                    </div>--}}
+                    <div class="row text-center">
+                        <div class="titulo-comofunciona">
+                            <h2>Capacitação</h2>
+                            <div class="capacitacao-video-heading">
+                                <div class="capacitacao-greatings">Olá <span  class="capacitacao-username">{{ $loggedUser->name }}</span></div>
+                                <div class="capacitacao-tit-video">{{ $lesson['title'] }}</div>
+                            </div>
+                        </div>
 
-                    {{--<div class="capacitacao-heading">Lorem ipsum dolor sit amet, consectetur adipiscing elit. In et tellus eget diam sagittis rutrum at at eros. Vestibulum vel efficitur nibh. Vestibulum consectetur, tortor sed feugiat rhoncus, nisi tortor semper risus, ut accumsan massa neque quis diam. In egestas eros et enim tempus, ac semper enim dignissim. Cras porta elit quis iaculis bibendum. Praesent pellentesque sapien odio, id aliquam metus dictum in. Maecenas dapibus lorem feugiat risus ultrices sollicitudin. Vivamus fringilla ullamcorper libero. Donec eget eleifend ligula. Etiam a consequat urna.</div>--}}
+                    </div>
+                </div>
+                <div class="col-sm-2">
+                    <div class="grow grid-item ecstasy-orange">
+                    </div>
+                </div>
+                <div class="col-sm-2">
+                    <div class="grow grid-item violet-red">
+                    </div>
                 </div>
             </div>
+
+
+
+
+
 
             <div class="container capacitacao-video">
                 <div class="row">
@@ -20,7 +59,7 @@
                         <div class='embed-container'><iframe src='{{ $lesson['video-url'] }}' frameborder='0' allowfullscreen></iframe></div>
                     </div>
 
-                    <a class="btn btn-danger" href="{{ route('training.index', ['year' => 2017]) }}">
+                    <a class="btn btn-danger btn-voltar" href="{{ route('training.index', ['year' => 2017]) }}">
                         <i class="fa fa-undo fa-lg"></i> Voltar </a>
                 </div>
             </div>
