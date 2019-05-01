856aee9e29bc4696e79b5016997066e9acfee72f
Breno Trengrouse <breno@gmail.com>
2019-05-01 12:36:55 -0300

    site 2019 - favicon

--- resources/views/2019/subscriptions/index.blade.php
+++ resources/views/2019/subscriptions/index.blade.php
@@ -3,12 +3,12 @@
 @section('contents')
 
 
-    <section class="bg-pattern subscribe-section masthead" id="form-subscribe">
+    <section class="bg-pattern subscribe-section " id="form-subscribe">
 
         <div class="container">
 
                 <div id="subscribe" class="form-subscribe" >
-                    <div class="row">
+                    <div class="row masthead">
                         <div class="col-12 text-center">
                             <h1 class="">Inscreva-se no Parlamento Juvenil {{ config('app.year') }}</h1>
                         </div>
