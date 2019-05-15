ddda984bb1c9c44275ae6ec1420d4a283697d065
Orlando Vinícius <orlando.vinicius1987@gmail.com>
2019-05-15 21:05:41 +0000

    Acerto na validação #issue #208

--- app/Http/Requests/LoginSeeducUser.php
+++ app/Http/Requests/LoginSeeducUser.php
@@ -23,7 +23,7 @@ class LoginSeeducUser extends Request
     {
         return [
             'registration' => 'required',
-            'birthdate'    => 'required|birthdate'
+            'birthdate'    => 'required|birthdate|lessthan18|morethan13'
         ];
     }
 }
