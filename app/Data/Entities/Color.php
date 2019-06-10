ca499736e3f9661982ca1d9e935ec23a40e57c84
Antonio Carlos Ribeiro <acr@antoniocarlosribeiro.com>
2019-06-10 15:46:24 -0300

    Add 2019 colors

--- app/Data/Entities/Color.php
+++ app/Data/Entities/Color.php
@@ -64,6 +64,56 @@ class Color
                     'color' => '#292C95',
                 ],
             ],
+            2019 => [
+                [
+                    'name' => 'rouge',
+                    'color' => '#A54686',
+                ],
+                [
+                    'name' => 'violet-red',
+                    'color' => '#BC1C91',
+                ],
+                [
+                    'name' => 'torch-red',
+                    'color' => '#FF141E',
+                ],
+                [
+                    'name' => 'ecstasy-orange',
+                    'color' => '#F58220',
+                ],
+                [
+                    'name' => 'supernova-yellow',
+                    'color' => '#FFCA00',
+                ],
+                [
+                    'name' => 'turbo-yellow',
+                    'color' => '#FFF000',
+                ],
+                [
+                    'name' => 'haze-green',
+                    'color' => '#00A648',
+                ],
+                [
+                    'name' => 'lima-green',
+                    'color' => '#91CE18',
+                ],
+                [
+                    'name' => 'cerulean-blue',
+                    'color' => '#0094DB',
+                ],
+                [
+                    'name' => 'danube-blue',
+                    'color' => '#5A8BC9',
+                ],
+                [
+                    'name' => 'endeavour-blue',
+                    'color' => '#0060AE',
+                ],
+                [
+                    'name' => 'jacksons-purple',
+                    'color' => '#292C95',
+                ],
+            ],
         ]);
     }
 
