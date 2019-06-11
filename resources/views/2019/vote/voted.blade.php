fba686914f11965cf0a8ce0004485d8e794c4fd0
Breno Trengrouse <breno@gmail.com>
2019-06-11 11:00:37 -0300

    refactor vote 002

--- resources/views/2019/vote/voted.blade.php
+++ resources/views/2019/vote/voted.blade.php
@@ -1,17 +1,33 @@
 @extends( get_current_year().'.layouts.layout')
 
 @section('contents')
-    <div class="alert alert-success text-center" role="alert">
-        <i class="fa fa-check" aria-hidden="true"></i>
-        <h1 class="title-alert">Seu voto foi validado!</h1>
-        <h3>Parabéns por participar do Parlamento Juvenil 2017</h3>
 
-        <br><br><br><br>
 
-        <h2>Alguém mais vai votar neste dispositivo? Clique no botão abaixo</h2>
 
-        <br><br>
+    <div class="container">
+        <div class="row">
+            <div class="offset-2 col-8">
 
-        <a href="/vote" class="btn btn-lg btn-danger">Votar</a>
+                <div class="alert alert-success text-center" role="alert">
+                    <i class="fa fa-check" aria-hidden="true"></i>
+                    <h1 class="title-alert">Seu voto foi validado!</h1>
+                    <h3>Parabéns por participar do Parlamento Juvenil 2019</h3>
+
+                    <h2 class="mt-5">Alguém mais vai votar neste dispositivo? <br> Clique no botão abaixo</h2>
+
+                    <a href="/vote" class="btn btn-lg btn-danger mt-5">Votar</a>
+                </div>
+
+            </div>
+        </div>
     </div>
+
+
+
+
+
+
+
+
+
 @stop
