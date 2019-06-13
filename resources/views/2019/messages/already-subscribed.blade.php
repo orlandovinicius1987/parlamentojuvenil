59053b657d6eba0ece604f8349725376fca6434b
Breno Trengrouse <breno@gmail.com>
2019-06-13 11:25:32 -0300

    refactor vote 006

--- resources/views/2019/messages/already-subscribed.blade.php
+++ resources/views/2019/messages/already-subscribed.blade.php
@@ -2,7 +2,7 @@
 
 @section('contents')
     <div id="subscribe" class="form-subscribe" >
-        <h1 class="text-centerx">Você já está inscrito!</h1>
+        <h1 class="text-center">Você já está inscrito!</h1>
 
         <div class="contact-text text-center">
             <p>Caso queira alterar algum dado, por favor, entre em contato com a gente:</p>
