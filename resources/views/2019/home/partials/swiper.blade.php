be651d02ac28f456e95a443ceaa61f8fd2d070be
Breno Trengrouse <breno@gmail.com>
2019-07-04 14:18:53 -0300

    add banner vote

--- resources/views/2019/home/partials/swiper.blade.php
+++ resources/views/2019/home/partials/swiper.blade.php
@@ -6,51 +6,46 @@
 
 <header class="text-center swiper">
     <div class="container">
-
-        {{--<div class="row">
-            <div class="col-md-12 col-lg-8 quehistoriaimg">
-                <img src="/templates/{{ get_current_year() }}/assets/img/site2016-mockup.png" class="img-fluid"><br>
-            </div>
-
-            <div class="col-md-12 col-lg-4 text-left quehistoria">
-                <h3 class="">Mas que história é essa?</h3>
-
-                <p class="lead"> O Parlamento Juvenil da ALERJ é dos primeiros projetos do Brasil que simula com os jovens o funcionamento do Legislativo.</p>
-                <p class="lead"> Criado em 1998 e reformulado pelos próprios Parlamentares Juvenis em 2015, chega em 2019 à sua 12&ordf; edição.</p>
-            </div>
-        </div>--}}
-
-
-
         <div class="row">
 
 
             @if (config('app.subscriptions.enabled') === true)
-            <div class="col-md-6 col-lg-8 text-left subscribe-banner-txt">
-                <h3 class="">Inscrições Abertas</h3>
+                <div class="col-md-6 col-lg-8 text-left subscribe-banner-txt">
+                    <h3 class="">Inscrições Abertas</h3>
 
-                <p class="lead">Até 7 de junho</p>
-                <p class="lead">Estudantes do 1&deg; e do 2&deg; anos do Ensino Médio da Rede Estadual com idade entre 14 e 17 anos.</p>
+                    <p class="lead">Até 7 de junho</p>
+                    <p class="lead">Estudantes do 1&deg; e do 2&deg; anos do Ensino Médio da Rede Estadual com idade entre 14 e 17 anos.</p>
 
-                <p class="lead">Importante!<br> Tenha o número de sua matrícula em mãos.</p>
+                    <p class="lead">Importante!<br> Tenha o número de sua matrícula em mãos.</p>
 
-                <a class="btn btn-primary btn-lg" href="/inscricao" role="button">  <i class="fas fa-plus-circle"></i> Inscreva-se Aqui! </a>
-            </div>
+                    <a class="btn btn-primary btn-lg" href="/inscricao" role="button">  <i class="fas fa-plus-circle"></i> Inscreva-se Aqui! </a>
+                </div>
 
 
-            <div class="col-md-6 col-lg-4 subscribe-banner-img ">
-                <img src="/templates/{{ get_current_year() }}/assets/img/subscribe.png" class="img-fluid">
-            </div>
-
+                <div class="col-md-6 col-lg-4 subscribe-banner-img ">
+                    <img src="/templates/{{ get_current_year() }}/assets/img/subscribe.png" class="img-fluid">
+                </div>
             @endif
 
+
+            <div class="d-none d-md-block col-md-5 col-lg-4 subscribe-banner-img ">
+                <img src="/templates/{{ get_current_year() }}/assets/img/vote.png" class="img-fluid">
+            </div>
+
+            <div class="col-md-7 col-lg-8 text-left subscribe-banner-txt">
+                <h3 class="">Votação do Segundo Turno</h3>
+
+                {{--<p class="lead">Até 7 de junho</p>--}}<br>
+                <p class="lead">Já está aberta a votação para o Segundo Turno do Parlamento Juvenil</p>
+                <br> &nbsp;<br> &nbsp;<br>
+
+
+                <a class="btn btn-primary btn-lg"  href="{{ route('vote.index') }} role="button">  <i class="fas fa-plus-circle"></i> Vote Aqui </a>
+            </div>
+
+
         </div>
-
-
     </div>
-
-
-
 </header>
 
 
@@ -124,16 +119,16 @@
             </div>
 --}}
 
-            {{--
-            <div class="swiper-slide">Slide 3</div>
-            <div class="swiper-slide">Slide 4</div>
-            <div class="swiper-slide">Slide 5</div>
-            <div class="swiper-slide">Slide 6</div>
-            <div class="swiper-slide">Slide 7</div>
-            <div class="swiper-slide">Slide 8</div>
-            <div class="swiper-slide">Slide 9</div>
-            <div class="swiper-slide">Slide 10</div>
-            --}}
+{{--
+<div class="swiper-slide">Slide 3</div>
+<div class="swiper-slide">Slide 4</div>
+<div class="swiper-slide">Slide 5</div>
+<div class="swiper-slide">Slide 6</div>
+<div class="swiper-slide">Slide 7</div>
+<div class="swiper-slide">Slide 8</div>
+<div class="swiper-slide">Slide 9</div>
+<div class="swiper-slide">Slide 10</div>
+--}}
 
         </div>
 
